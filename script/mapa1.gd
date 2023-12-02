0ed8d300f70ec719304e39540b0ac0acd07e9cbd
Luanvraft <luanbarroso1212@gmail.com>
2023-12-02 14:03:30 -0300

    literatura Brasileira adicionada

--- script/mapa1.gd
+++ script/mapa1.gd
@@ -1,25 +1,41 @@
 extends Node
 
 var ColetaLivro = 0
+var overlay_scene_instance = null
 
+func _ready():
+	# Carrega a cena de sobreposição
+	overlay_scene_instance = preload("res://scenes/livro_1.tscn").instantiate()
+	# Adiciona a cena de sobreposição como filho do controlador, mas a mantém invisível
+	add_child(overlay_scene_instance)
+	overlay_scene_instance.visible = false
+	
 func _process(_delta):
 	if ColetaLivro == 1:
 		if Input.is_action_pressed("ui_accept"):
-			var overlay_controller = get_node("macunaima/Livro2")
-			var player = get_node("CharacterBody2D")
-			overlay_controller.hide_overlay()
+			var player = get_node("player")
+			hide_overlay()
 			player.Play_Player()
 			ColetaLivro = 0
 		pass
 
+func show_overlay():
+	# Torna a cena de sobreposição visível
+	overlay_scene_instance.visible = true
+	overlay_scene_instance.set_process_input(true)
 
-func _on_dom_casmurro_body_entered(body):
+func hide_overlay():
+	# Torna a cena de sobreposição invisível
+	overlay_scene_instance.visible = false
+	overlay_scene_instance.set_process_input(false)
+
+func _on_dom_casmurro_body_entered(_body):
 	if ColetaLivro == 0:
-		var overlay_controller = get_node("dom_casmurro/Livro1")
-		var player = get_node("CharacterBody2D")
+		var player = get_node("player")
 		var livro = get_node("dom_casmurro")
-		overlay_controller.show_overlay()
+		overlay_scene_instance.position = Vector2i(575, -45)
+		show_overlay()
 		player.Stop_Player()
-		livro.hidden()
+		livro.queue_free()
 		ColetaLivro = 1
 	pass # Replace with function body.
--- script/mapa2.gd
+++ script/mapa2.gd
@@ -1,43 +1,73 @@
 extends Node
 
 var ColetaLivro = 0
+var overlay_scene_instance1 = null
+var overlay_scene_instance2 = null
+
+
+func _ready():
+	overlay_scene_instance1 = preload("res://scenes/livro_2.tscn").instantiate()
+	add_child(overlay_scene_instance1)
+	overlay_scene_instance1.visible = false
+	overlay_scene_instance2 = preload("res://scenes/livro_3.tscn").instantiate()
+	add_child(overlay_scene_instance2)
+	overlay_scene_instance2.visible = false
 
 func _process(_delta):
 	if ColetaLivro == 1:
 		if Input.is_action_pressed("ui_accept"):
-			var overlay_controller = get_node("macunaima/Livro2")
 			var player = get_node("CharacterBody2D")
-			overlay_controller.hide_overlay()
+			hide_overlay1()
 			player.Play_Player()
 			ColetaLivro = 0
 		pass
 	elif ColetaLivro == 2:
 		if Input.is_action_pressed("ui_accept"):
-			var overlay_controller = get_node("o_cortico/Livro3")
 			var player = get_node("CharacterBody2D")
-			overlay_controller.hide_overlay()
+			hide_overlay2()
 			player.Play_Player()
 			ColetaLivro = 0
 		pass
-func _on_macunaima_body_entered():
+
+func show_overlay1():
+	# Torna a cena de sobreposição visível
+	overlay_scene_instance1.visible = true
+	overlay_scene_instance1.set_process_input(true)
+
+func hide_overlay1():
+	# Torna a cena de sobreposição invisível
+	overlay_scene_instance1.visible = false
+	overlay_scene_instance1.set_process_input(false)
+
+func show_overlay2():
+	# Torna a cena de sobreposição visível
+	overlay_scene_instance2.visible = true
+	overlay_scene_instance2.set_process_input(true)
+
+func hide_overlay2():
+	# Torna a cena de sobreposição invisível
+	overlay_scene_instance2.visible = false
+	overlay_scene_instance2.set_process_input(false)
+
+func _on_macunaima_body_entered(_body):
 	if ColetaLivro == 0:
-		var overlay_controller = get_node("macunaima/Livro2")
 		var player = get_node("CharacterBody2D")
 		var livro = get_node("macunaima")
-		overlay_controller.show_overlay()
+		overlay_scene_instance1.position = Vector2i(1640, -220)
+		show_overlay1()
 		player.Stop_Player()
-		livro.hidden()
+		livro.queue_free()
 		ColetaLivro = 1
 	pass
 
-
-func _on_o_cortico_body_entered():
+func _on_o_cortico_body_entered(_body):
 	if ColetaLivro == 0:
-		var overlay_controller = get_node("o_cortico/Livro3")
 		var player = get_node("CharacterBody2D")
 		var livro = get_node("o_cortico")
-		overlay_controller.show_overlay()
+		overlay_scene_instance2.position = Vector2i(2927, -225)
+		show_overlay2()
 		player.Stop_Player()
-		livro.hidden()
+		livro.queue_free()
 		ColetaLivro = 2
-	pass # Replace with function body.
+	pass 
+	
